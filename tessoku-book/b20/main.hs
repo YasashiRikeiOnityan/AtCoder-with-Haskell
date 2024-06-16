85670aa7b6b61acd5a93dcf68f7bc37f18054777
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-17 06:42:52 +0900

    Edit Distance（TLE）

--- tessoku-book/b20/main.hs
+++ tessoku-book/b20/main.hs
@@ -1,14 +1,25 @@
 module Main where
 
-import Control.Monad ( replicateM )
+type EDProblem = (String, String)
+type Distance = Int
 
-main :: IO ()
-main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
+editDistance :: EDProblem -> Distance
+editDistance ([], []) = 0
+editDistance (x : xs, []) = 1 + editDistance (xs, [])
+editDistance ([], y : ys) = 1 + editDistance ([], ys)
+editDistance (x : xs, y : ys) = 
+    minimum [
+        s + editDistance (xs, ys),
+        1 + editDistance (x : xs, ys),
+        1 + editDistance (xs, y : ys)
+    ] where s = if x == y then 0 else 1
 
 pair :: [a] -> (a, a)
 pair [x, y] = (x, y)
 pair _ = (undefined, undefined)
+
+main :: IO ()
+main = do
+    s <- getLine
+    t <- getLine
+    print $ editDistance (s, t)
