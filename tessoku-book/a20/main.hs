b88b116d5277e3cae2c6d864590a7ebac6db8d92
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-17 06:42:38 +0900

    LCSの実装（未理解）

--- tessoku-book/a20/main.hs
+++ tessoku-book/a20/main.hs
@@ -1,14 +1,28 @@
 module Main where
 
-import Control.Monad ( replicateM )
+type Vect a = ([a], Int)
+
+nil :: Vect a
+nil = ([], 0)
+
+cons :: a -> Vect a -> Vect a
+cons x (xs, n) = (x : xs, succ n)
+
+size :: Vect a -> Int
+size = snd
+
+maxVect :: Vect a -> Vect a -> Vect a
+maxVect xs@(_, m) ys@(_, n)
+    | m > n     = xs
+    | otherwise = ys
+
+lcs :: Eq a => [a] -> [a] -> Vect a
+lcs s t = head $ foldr phi (replicate (succ (length s)) nil) t
+    where
+        phi y row = foldr (step y) [nil] (zip3 s row (tail row))
+        step y (x, cs1, cs2) row
+            | x == y    = cons x cs2 : row
+            | otherwise = maxVect cs1 (head row) : row
 
 main :: IO ()
-main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+main = getLine >>= \s -> getLine >>= \t -> print (size (lcs s t))
