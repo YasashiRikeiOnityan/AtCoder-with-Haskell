0164291763bbd74b407843bb9103c9a5e410edc3
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-06-17 15:57:50 +0900

    自分用に下方修正

--- tessoku-book/a20/main.hs
+++ tessoku-book/a20/main.hs
@@ -1,28 +1,14 @@
 module Main where
 
-type Vect a = ([a], Int)
+import Data.Bifunctor ( bimap )
 
-nil :: Vect a
-nil = ([], 0)
-
-cons :: a -> Vect a -> Vect a
-cons x (xs, n) = (x : xs, succ n)
-
-size :: Vect a -> Int
-size = snd
-
-maxVect :: Vect a -> Vect a -> Vect a
-maxVect xs@(_, m) ys@(_, n)
-    | m > n     = xs
-    | otherwise = ys
-
-lcs :: Eq a => [a] -> [a] -> Vect a
-lcs s t = head $ foldr phi (replicate (succ (length s)) nil) t
+lcs :: [Char] -> [Char] -> ([Char], Int)
+lcs s t = head $ foldr phi (replicate (succ (length s)) ("", 0)) t
     where
-        phi y row = foldr (step y) [nil] (zip3 s row (tail row))
+        phi y row = foldr (step y) [("", 0)] (zip3 s row (tail row))
         step y (x, cs1, cs2) row
-            | x == y    = cons x cs2 : row
-            | otherwise = maxVect cs1 (head row) : row
+            | x == y    = bimap (y:) (1+) cs2 : row
+            | otherwise = (if snd cs1 > snd (head row) then cs1 else head row) : row
 
 main :: IO ()
-main = getLine >>= \s -> getLine >>= \t -> print (size (lcs s t))
+main = getLine >>= \s -> getLine >>= \t -> print (snd (lcs s t))
