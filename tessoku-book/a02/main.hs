f67db425f57e84d7bd7264a70ff6655ce0fc5934
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-06-19 17:38:52 +0900

    $と<$>の違いが分かりやすい

--- tessoku-book/a02/main.hs
+++ tessoku-book/a02/main.hs
@@ -1,14 +1,10 @@
 module Main where
 
-import Control.Monad ( replicateM )
+yn :: Bool -> String
+yn b = if b then "Yes" else "No"
 
 main :: IO ()
 main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    [_, x] <- map read . words <$> getLine :: IO [Int]
+    as <- map read . words <$> getLine :: IO [Int]
+    putStrLn . yn . elem x $ as
