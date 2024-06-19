f35fe3ad18deace304fb536ece039268b4070b89
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-06-19 17:40:37 +0900

    初めてのリスト内方表記

--- tessoku-book/b02/main.hs
+++ tessoku-book/b02/main.hs
@@ -1,14 +1,9 @@
 module Main where
 
-import Control.Monad ( replicateM )
+yn :: Bool -> String
+yn b = if b then "Yes" else "No"
 
 main :: IO ()
 main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    [a, b] <- map read . words <$> getLine :: IO [Int]
+    putStrLn . yn . not . null $ [x | x <- [a..b], 100 `mod` x == 0]
