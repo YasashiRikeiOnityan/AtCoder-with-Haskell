f4fa88a0132462cd3482f984ef0725ef8baec163
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-06-19 17:38:08 +0900

    do記法使わない方が見やすいかもしれない

--- tessoku-book/b01/main.hs
+++ tessoku-book/b01/main.hs
@@ -1,14 +1,4 @@
 module Main where
 
-import Control.Monad ( replicateM )
-
 main :: IO ()
-main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+main = print . sum . map read . words =<< getLine
