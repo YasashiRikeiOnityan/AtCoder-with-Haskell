25cc9e06676f72528062ef978b5819b8d1bf6404
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-06-19 17:41:50 +0900

    zip [1..]でリストの要素に番号を振られる

--- tessoku-book/b03/main.hs
+++ tessoku-book/b03/main.hs
@@ -1,14 +1,13 @@
 module Main where
 
-import Control.Monad ( replicateM )
+b03 :: [(Int, Int)] -> Bool
+b03 as = 1000 `elem` [x + y + z | (i, x) <- as, (j, y) <- as, i /= j, (k, z) <- as, j /= k, k /= i]
+
+yn :: Bool -> String
+yn b = if b then "Yes" else "No"
 
 main :: IO ()
 main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    _ <- getLine
+    as <- map read . words <$> getLine
+    putStrLn . yn . b03 . zip [1..] $ as
