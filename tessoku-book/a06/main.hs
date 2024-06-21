490a583959602b200bbe3d8183a9c8f0a5315056
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-22 05:19:10 +0900

    mapM_を使う方が簡潔かな

--- tessoku-book/a06/main.hs
+++ tessoku-book/a06/main.hs
@@ -1,14 +1,21 @@
 module Main where
 
 import Control.Monad ( replicateM )
+import Data.Array ( Array, (!), listArray )
+
+cumulativeSum :: Int -> Int -> [Int] -> Array Int Int
+cumulativeSum l r = listArray (l, r) . scanl (+) 0
+
+numOfVisitors :: Array Int Int -> (Int, Int) -> Int
+numOfVisitors s (l, r) = s ! r - s ! pred l
+
+pair :: [Int] -> (Int, Int)
+pair [x, y] = (x, y)
+pair _ = undefined
 
 main :: IO ()
 main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    [n, q] <- map read . words <$> getLine
+    as <- map read . words <$> getLine
+    lrs <- map (pair . map read . words) <$> replicateM q getLine
+    mapM_ (print . numOfVisitors (cumulativeSum 0 n as)) lrs
