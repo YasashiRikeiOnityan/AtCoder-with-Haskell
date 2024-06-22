f1fcea1a88db6df348aeb534b81ae804492bb12a
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-23 00:28:16 +0900

    最初と最後は切り捨てて出力する

--- tessoku-book/a07/main.hs
+++ tessoku-book/a07/main.hs
@@ -1,14 +1,18 @@
 module Main where
 
 import Control.Monad ( replicateM )
+import Data.Array ( accumArray, elems, Array )
+
+add2PreviousDay :: [Int] -> [(Int, Int)]
+add2PreviousDay [l, r] = [(l, 1), (succ r, -1)]
+add2PreviousDay _ = []
+
+cumulativeSum :: Array Int Int -> [Int]
+cumulativeSum = scanl (+) 0 . elems
 
 main :: IO ()
 main = do
+    d <- readLn
     n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    lrs <- concatMap (add2PreviousDay . fmap read . words) <$> replicateM n getLine
+    mapM_ print . tail . init . cumulativeSum . accumArray (+) 0 (1, succ d) $ lrs
