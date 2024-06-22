7b636575093ebafc9162f4a9476d7160e8bad211
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-23 00:27:51 +0900

    a06とほとんど同じ

--- tessoku-book/b06/main.hs
+++ tessoku-book/b06/main.hs
@@ -1,14 +1,31 @@
 module Main where
 
 import Control.Monad ( replicateM )
+import Data.Array ( Array, (!), listArray )
+
+cumulativeSum :: (Int, Int) -> [Int] -> Array Int Int
+cumulativeSum lr = listArray lr . scanl (+) 0
+
+luckUnluck :: Array Int Int -> (Int, Int) -> (Int, Int)
+luckUnluck s (l, r) = (lucky, unlucky)
+    where
+        lucky = s ! r - s ! pred l
+        unlucky = r - l + 1 - lucky
+
+pair :: [Int] -> (Int, Int)
+pair [x, y] = (x, y)
+pair _ = undefined
+
+out :: (Int, Int) -> String
+out (lucky, unlucky)
+    | lucky > unlucky  = "win"
+    | lucky == unlucky = "draw"
+    | otherwise        = "lose"
 
 main :: IO ()
 main = do
     n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    as <- map read . words <$> getLine
+    q <- readLn
+    lrs <- map (pair . map read . words) <$> replicateM q getLine
+    mapM_ (putStrLn . out . luckUnluck (cumulativeSum (0, n) as)) lrs
