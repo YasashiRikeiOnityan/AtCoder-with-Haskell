a14443f0702661eeb958ed0a8c25b2ae76a19990
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-23 00:28:27 +0900

    a07とちょっとだけ違う

--- tessoku-book/b07/main.hs
+++ tessoku-book/b07/main.hs
@@ -1,14 +1,18 @@
 module Main where
 
 import Control.Monad ( replicateM )
+import Data.Array ( accumArray, elems, Array )
+
+attendAndLeave :: [Int] -> [(Int, Int)]
+attendAndLeave [l, r] = [(l, 1), (r, -1)]
+attendAndLeave _ = []
+
+cumulativeSum :: Array Int Int -> [Int]
+cumulativeSum = scanl (+) 0 . elems
 
 main :: IO ()
 main = do
+    t <- readLn
     n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    lrs <- concatMap (attendAndLeave . map read . words) <$> replicateM n getLine
+    mapM_ print . tail . init . cumulativeSum . accumArray (+) 0 (0, t) $ lrs
