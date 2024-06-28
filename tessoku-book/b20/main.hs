48e988b3e6e4f49180bbe61f335407e498a53aee
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-06-28 14:46:26 +0900

    同じもの提出してもACとTLEを得た。ギリギリなところ。制限時間2secにしてよ...

--- tessoku-book/b20/main.hs
+++ tessoku-book/b20/main.hs
@@ -1,25 +1,49 @@
 module Main where
 
-type EDProblem = (String, String)
-type Distance = Int
+import qualified Data.ByteString.Char8 as B
+import Data.Array ( Array, array, (!) ,range )
 
-editDistance :: EDProblem -> Distance
-editDistance ([], []) = 0
-editDistance (x : xs, []) = 1 + editDistance (xs, [])
-editDistance ([], y : ys) = 1 + editDistance ([], ys)
-editDistance (x : xs, y : ys) = 
-    minimum [
-        s + editDistance (xs, ys),
-        1 + editDistance (x : xs, ys),
-        1 + editDistance (xs, y : ys)
-    ] where s = if x == y then 0 else 1
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+editDistance :: B.ByteString -> B.ByteString -> Int
+editDistance xs ys = table ! (m,n)
+    where
+    (m,n) = (B.length xs, B.length ys)
+    x     = array (1,m) (zip [1..] (B.unpack xs))
+    y     = array (1,n) (zip [1..] (B.unpack ys))
+    
+    table :: Array (Int,Int) Int
+    table = array bnds [(ij, dist ij) | ij <- range bnds]
+    bnds  = ((0,0),(m,n))
+    
+    dist (0,j) = j
+    dist (i,0) = i
+    dist (i,j) = minimum [table ! (i-1,j) + 1, table ! (i,j-1) + 1,
+        if x ! i == y ! j then table ! (i-1,j-1) else 1 + table ! (i-1,j-1)]
 
 main :: IO ()
-main = do
-    s <- getLine
-    t <- getLine
-    print $ editDistance (s, t)
+main = B.getLine >>= \s -> B.getLine >>= \t -> print $ editDistance s t
+{-
+module Main where
+
+
+import qualified Data.ByteString.Char8 as B
+import Data.Array ( Array, listArray, (!) ,range )
+
+editDistance :: B.ByteString -> B.ByteString -> Int
+editDistance xs ys = table ! (m, n)
+    where
+        (m, n) = (B.length xs, B.length ys)
+        x      = listArray (1, m) (B.unpack xs)
+        y      = listArray (1, n) (B.unpack ys)
+
+        table :: Array (Int, Int) Int
+        table = listArray bnds [dist ij | ij <- range bnds]
+        bnds  = ((0, 0), (m, n))
+
+        dist (0, j) = j
+        dist (i, 0) = i
+        dist (i, j) = minimum [succ (table ! (pred i, j)), succ (table ! (i, pred j)),
+            if x ! i == y ! j then table ! (pred i, pred j) else succ (table ! (pred i, pred j))]
+
+main :: IO ()
+main = B.getLine >>= \s -> B.getLine >>= \t -> print $ editDistance s t
+-}
