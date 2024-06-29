435d0601c69f2fd6a3d011749e06e7a68e6d7d95
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-29 09:32:53 +0900

    だんだんコツが掴めてきた

--- tessoku-book/b20/main.hs
+++ tessoku-book/b20/main.hs
@@ -1,49 +1,34 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as B
-import Data.Array ( Array, array, (!) ,range )
-
-editDistance :: B.ByteString -> B.ByteString -> Int
-editDistance xs ys = table ! (m,n)
-    where
-    (m,n) = (B.length xs, B.length ys)
-    x     = array (1,m) (zip [1..] (B.unpack xs))
-    y     = array (1,n) (zip [1..] (B.unpack ys))
-    
-    table :: Array (Int,Int) Int
-    table = array bnds [(ij, dist ij) | ij <- range bnds]
-    bnds  = ((0,0),(m,n))
-    
-    dist (0,j) = j
-    dist (i,0) = i
-    dist (i,j) = minimum [table ! (i-1,j) + 1, table ! (i,j-1) + 1,
-        if x ! i == y ! j then table ! (i-1,j-1) else 1 + table ! (i-1,j-1)]
-
-main :: IO ()
-main = B.getLine >>= \s -> B.getLine >>= \t -> print $ editDistance s t
-{-
-module Main where
-
-
-import qualified Data.ByteString.Char8 as B
-import Data.Array ( Array, listArray, (!) ,range )
+import Data.Array ( Array, listArray, (!), range )
+import Data.List ( foldl', scanl' )
 
 editDistance :: B.ByteString -> B.ByteString -> Int
 editDistance xs ys = table ! (m, n)
     where
-        (m, n) = (B.length xs, B.length ys)
-        x      = listArray (1, m) (B.unpack xs)
-        y      = listArray (1, n) (B.unpack ys)
+    (m, n) = (B.length xs, B.length ys)
 
-        table :: Array (Int, Int) Int
-        table = listArray bnds [dist ij | ij <- range bnds]
-        bnds  = ((0, 0), (m, n))
+    x = listArray (1, m) (B.unpack xs)
+    y = listArray (1, n) (B.unpack ys)
+    
+    table :: Array (Int, Int) Int
+    table = listArray bnds [dist ij | ij <- range bnds]
+    bnds  = ((0, 0), (m, n))
+    
+    dist (0, j) = j
+    dist (i, 0) = i
+    dist (i, j) = minimum [
+            table ! (i-1, j) + 1, 
+            table ! (i ,j-1) + 1,
+            if x ! i == y ! j then table ! (i-1, j-1) else 1 + table ! (i-1, j-1)
+        ]
 
-        dist (0, j) = j
-        dist (i, 0) = i
-        dist (i, j) = minimum [succ (table ! (pred i, j)), succ (table ! (i, pred j)),
-            if x ! i == y ! j then table ! (pred i, pred j) else succ (table ! (pred i, pred j))]
-
+editDistance' :: String -> String -> Int
+editDistance' s t = last . foldl' phi [0..length t] . zip [1..] $ s
+    where
+        phi acc (i, a) = scanl' (step a) i $ zip3 t acc (tail acc)
+        step a i (b, j, k) = minimum [succ i, (if a == b then succ else id) j, succ k]
+ 
 main :: IO ()
-main = B.getLine >>= \s -> B.getLine >>= \t -> print $ editDistance s t
--}
+main = getLine >>= \s -> getLine >>= \t -> print $ editDistance' s t
