10084b7e45ed6bfc9d1179aa37c0237068e23d53
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-30 08:19:33 +0900

    いま考慮すべき範囲がどうなってるか？に苦戦した...

--- tessoku-book/a21/main.hs
+++ tessoku-book/a21/main.hs
@@ -1,14 +1,29 @@
 module Main where
 
 import Control.Monad ( replicateM )
+import Data.Array ( Array, listArray, (!), inRange )
 
-main :: IO ()
-main = do
-    n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
+blockGame :: Int -> Array Int (Int, Int) -> Int
+blockGame n parr = maximum [table ! l ! (n - l) | l <- [0..n]]
+    where
+        table = listArray (0, n) [listArray (0, n - l) [score l r | r <- [0..n-l]] | l <- [0..n]]
+
+        score 0 0 = 0
+        score l r
+            | l == 0 = score2
+            | r == 0 = score1
+            | otherwise = max score1 score2
+            where
+                score1 = table ! pred l ! r + if inRange (l, n - r) (fst (parr ! l)) then snd (parr ! l) else 0
+                score2 = table ! l ! pred r + if inRange (succ l, n - pred r) (fst (parr ! (n - pred r))) then snd (parr ! (n - pred r)) else 0
+
 
 pair :: [a] -> (a, a)
 pair [x, y] = (x, y)
 pair _ = (undefined, undefined)
+
+main :: IO ()
+main = do
+    n <- readLn
+    pas <- map (pair . map read . words) <$> replicateM n getLine
+    print . blockGame n . listArray (1, n) $ pas
