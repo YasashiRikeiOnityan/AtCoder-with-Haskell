7631c7916bab05e7d0a43876c4e7313db22dd3b9
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2024-06-30 15:09:24 +0900

    dpの見やすくて汎用性のある解き方を習得した

--- tessoku-book/a16/main.hs
+++ tessoku-book/a16/main.hs
@@ -1,14 +1,25 @@
 module Main where
 
-import Control.Monad ( replicateM )
+import Data.Array ( listArray, (!) )
+
+dungeon :: Int -> [Int] -> [Int] -> Int
+dungeon n xs ys = dp ! n
+    where
+        xrr = listArray (1, pred n) xs
+        yrr = listArray (1, (pred . pred) n) ys
+
+        dp = listArray (1, n) [cost i | i <- [1..n]]
+
+        cost 1 = 0
+        cost 2 = xrr ! 1
+        cost i = min (dp ! s + xrr ! s) (dp ! t + yrr ! t)
+            where
+                s = pred i
+                t = (pred . pred) i
 
 main :: IO ()
 main = do
     n <- readLn
-    [x, y] <- map read . words <$> getLine
-    xys <- map (pair . map read . words) <$> replicateM q getLine
-    print 0
-
-pair :: [a] -> (a, a)
-pair [x, y] = (x, y)
-pair _ = (undefined, undefined)
+    as <- map read . words <$> getLine
+    bs <- map read . words <$> getLine
+    print $ dungeon n as bs
